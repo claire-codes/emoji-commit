a209eccb434d6c885dbc90997bace9d0030e65df
Claire Parker <claire.parker87@gmail.com>
2015-12-05 20:16:18 +0000

    :clapper: Consistent module names

--- lib/emoji-commit-msg.rb
+++ lib/emoji-commit-msg.rb
@@ -1,31 +1,32 @@
 #!/usr/bin/ruby
 require 'json'
 
-class Emoji_Commit
+module EmojiCommit
+  class Emoji_Commit
 
-  attr_accessor :msg_file
+    attr_accessor :msg_file
 
-  def initialize(file)
-    @msg_file = file
-  end
+    def initialize(file)
+      @msg_file = file
+    end
 
-  def emoji_exists?(message)
-    message.match(/^:(-|e-\w+)?(\w+):/) ? true : false
-  end
+    def emoji_exists?(message)
+      message.match(/^:(-|e-\w+)?(\w+):/) ? true : false
+    end
 
-  def get_emoji
-    puts Dir.pwd
-    
-    emojis_file = File.read("#{Dir.pwd}/.git/hooks/emojis.json")
-    emojis_hash = JSON.parse(emojis_file)
-    emojis_hash[emojis_hash.keys.sample].strip
-  end
+    def get_emoji
+      puts Dir.pwd
 
-  def edit_commit_msg
-    commit_msg = File.read(@msg_file)
-    unless emoji_exists?(commit_msg)
-      File.write(@msg_file, "#{get_emoji} #{commit_msg}")
+      emojis_file = File.read("#{Dir.pwd}/.git/hooks/emojis.json")
+      emojis_hash = JSON.parse(emojis_file)
+      emojis_hash[emojis_hash.keys.sample].strip
+    end
+
+    def edit_commit_msg
+      commit_msg = File.read(@msg_file)
+      unless emoji_exists?(commit_msg)
+        File.write(@msg_file, "#{get_emoji} #{commit_msg}")
+      end
     end
   end
-
 end
--- lib/emoji-script.rb
+++ lib/emoji-script.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/ruby
 require 'emoji-commit-msg'
 
-foo = Emoji_Commit.new ARGV[0]
-foo.edit_commit_msg
+EmojiCommit::Emoji_Commit.new(ARGV[0]).edit_commit_msg

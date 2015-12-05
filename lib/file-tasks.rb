8940018a176f759cf4ce0c3aae1809a6eb89b7b0
Claire Parker <claire.parker87@gmail.com>
2015-12-05 19:46:36 +0000

    :grimacing: Rubocopping

--- lib/file-tasks.rb
+++ lib/file-tasks.rb
@@ -3,31 +3,30 @@ require 'FileUtils'
 require 'Thor'
 
 module EmojiCommit
-	class Cli < Thor
-		desc 'install', 'installs commit hook scripts'
+  class Cli < Thor
+    desc 'install', 'installs commit hook scripts'
 
-		def install
-			exit unless Dir.exists?('.git')
+    def install
+      exit unless Dir.exist?('.git')
 
-			if File.exists?('.git/hooks/commit-msg') then FileUtils.rm('.git/hooks/commit-msg') end
+      if File.exist?('.git/hooks/commit-msg') then FileUtils.rm('.git/hooks/commit-msg') end
 
-			if File.exists?('.git/hooks/commit-msg.sample') then FileUtils.rm('.git/hooks/commit-msg.sample') end
+      if File.exist?('.git/hooks/commit-msg.sample') then FileUtils.rm('.git/hooks/commit-msg.sample') end
 
-			path = path_to_resources
+      path = path_to_resources
 
-			FileUtils.cp(path + '/emoji-script.rb', '.git/hooks/emoji-script.rb')
-			FileUtils.cp(path + '/emoji-commit-msg.rb', '.git/hooks/emoji-commit-msg.rb')
-			FileUtils.cp(path + '/commit-msg', '.git/hooks/commit-msg')
-			FileUtils.cp(path + '/assets/emojis.json', '.git/hooks/emojis.json')
-			FileUtils.chmod 0755, %w(.git/hooks/emoji-script.rb .git/hooks/emoji-commit-msg.rb .git/hooks/commit-msg .git/hooks/emojis.json)
-			puts 'Made it'
+      FileUtils.cp(path + '/emoji-script.rb', '.git/hooks/emoji-script.rb')
+      FileUtils.cp(path + '/emoji-commit-msg.rb', '.git/hooks/emoji-commit-msg.rb')
+      FileUtils.cp(path + '/commit-msg', '.git/hooks/commit-msg')
+      FileUtils.cp(path + '/assets/emojis.json', '.git/hooks/emojis.json')
+      FileUtils.chmod 0755, %w(.git/hooks/emoji-script.rb .git/hooks/emoji-commit-msg.rb .git/hooks/commit-msg .git/hooks/emojis.json)
+      puts 'Made it'
+    end
 
-		end
-
-		no_commands do
-			def path_to_resources
-				File.dirname(File.expand_path(__FILE__))
-			end
-		end			
-	end
+    no_commands do
+      def path_to_resources
+        File.dirname(File.expand_path(__FILE__))
+      end
+    end
+  end
 end

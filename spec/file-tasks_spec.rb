e0735921c7fa223dc76b59a0559f2c21cd716694
Claire Parker <claire.parker87@gmail.com>
2015-12-09 21:44:47 +0000

    :herb: Added SimpleCov

--- spec/file-tasks_spec.rb
+++ spec/file-tasks_spec.rb
@@ -4,10 +4,26 @@ describe EmojiCommit::Cli do
   describe '::install' do
     it 'will exit if there isn\'t a .git directory' do
       allow(Dir).to receive(:exist?).with('.git').and_return(false)
-      allow(STDIN).to receive(:gets).and_return('y')
       expect do
         expect(EmojiCommit::Cli.new.install).to output('Git has not been initialised in this directory. Bye').to_stdout
       end.to raise_error(SystemExit)
     end
+
+    it 'will exit if user types no' do
+      allow(Dir).to receive(:exist?).with('.git').and_return(true)
+      allow(STDIN).to receive(:gets).and_return('n')
+      expect do
+        expect(EmojiCommit::Cli.new.install).to output('Fine whatever. Bye').to_stdout
+      end.to raise_error(SystemExit)
+    end
+
+    it 'will exit if user types something other than y or n' do
+      allow(Dir).to receive(:exist?).with('.git').and_return(true)
+      allow(STDIN).to receive(:gets).and_return('?$@*')
+      expect do
+        expect(EmojiCommit::Cli.new.install).to output('Pardon? Oh who cares. Bye').to_stdout
+      end.to raise_error(SystemExit)
+    end
+
   end
 end

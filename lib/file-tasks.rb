7f57394fbd06648d687b612bccc23be2c4c25a60
Claire Parker <claire.parker87@gmail.com>
2015-12-11 12:25:53 +0000

    :fu: ::uninstall added

--- lib/file-tasks.rb
+++ lib/file-tasks.rb
@@ -7,11 +7,8 @@ module EmojiCommit
     desc 'install', 'installs commit hook scripts'
 
     def install
-      unless Dir.exist?('.git')
-        puts 'Git has not been initialised in this directory. Bye'
-        exit
-      end
-
+      no_git
+      
       puts 'You are about to overwrite any existing Git commit hook with the emoji script'
       puts 'Is that OK? (y|n)'
       
@@ -28,17 +25,50 @@ module EmojiCommit
       end
 
       if File.exist?('.git/hooks/commit-msg') then FileUtils.rm('.git/hooks/commit-msg') end
-      if File.exist?('.git/hooks/commit-msg.sample') then FileUtils.rm('.git/hooks/commit-msg.sample') end
 
       path = File.dirname(File.expand_path(__FILE__))    
 
-      ['emoji-script.rb', 'emoji-commit-msg.rb', 'commit-msg', 'emojis.json'].each do |f|
-        FileUtils.cp("#{path}/#{f}", ".git/hooks/#{f}")
+      ['emoji-script.rb', 'emoji-commit-msg.rb', 'commit-msg', 'emojis.json'].each do |filename|
+        FileUtils.cp("#{path}/#{filename}", ".git/hooks/#{filename}")
       end
       
       FileUtils.chmod 0755, %w(.git/hooks/emoji-script.rb .git/hooks/emoji-commit-msg.rb .git/hooks/commit-msg .git/hooks/emojis.json)
       
       puts 'Installed scripts successfully. Commit emoji-ful messages!'
+    end
+
+    def uninstall
+      no_git
+
+      puts 'You are about to uninstall the emoji Git commit hook'
+      puts 'Is that OK? (y|n)'
+
+      answer = STDIN.gets.strip.downcase
+
+      if answer == 'n'
+        puts 'Good choice. Bye'
+        exit
+      elsif answer != 'y'
+        puts 'Pardon? Oh who cares. Bye'
+        exit
+      else
+        puts 'As long as you\'re sure :( Removing files now'
+      end
+
+      ['emoji-script.rb', 'emoji-commit-msg.rb', 'commit-msg', 'emojis.json'].each do |filename|
+        FileUtils.rm(".git/hooks/#{filename}")
+      end
+
+      puts 'Uninstall scripts successfully. Enjoy your boring emoji-less life.'
+    end
+
+    no_commands do
+      def no_git
+        unless Dir.exist?('.git')
+          puts 'Git has not been initialised in this directory. Bye'
+          exit
+        end
+      end
     end
   end
 end
--- spec/file-tasks_spec.rb
+++ spec/file-tasks_spec.rb
@@ -24,6 +24,30 @@ describe EmojiCommit::Cli do
         expect(EmojiCommit::Cli.new.install).to output('Pardon? Oh who cares. Bye').to_stdout
       end.to raise_error(SystemExit)
     end
+  end
 
+  describe '::uninstall' do
+    it 'will exit if there isn\'t a .git directory' do
+      allow(Dir).to receive(:exist?).with('.git').and_return(false)
+      expect do
+        expect(EmojiCommit::Cli.new.uninstall).to output('Git has not been initialised in this directory. Bye').to_stdout
+      end.to raise_error(SystemExit)
+    end
+
+    it 'will exit if user types no' do
+      allow(Dir).to receive(:exist?).with('.git').and_return(true)
+      allow(STDIN).to receive(:gets).and_return('n')
+      expect do
+        expect(EmojiCommit::Cli.new.uninstall).to output('Good choice. Bye').to_stdout
+      end.to raise_error(SystemExit)
+    end
+
+    it 'will exit if user types something other than y or n' do
+      allow(Dir).to receive(:exist?).with('.git').and_return(true)
+      allow(STDIN).to receive(:gets).and_return('?$@*')
+      expect do
+        expect(EmojiCommit::Cli.new.uninstall).to output('Pardon? Oh who cares. Bye').to_stdout
+      end.to raise_error(SystemExit)
+    end
   end
 end

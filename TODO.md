87a7d1a0ab9ce3efb0aa0b7d407f83e2dde0f450
Claire Parker <claire.parker87@gmail.com>
2015-12-11 15:38:29 +0000

    :metro: Tests are _actually_ testing now

--- TODO.md
+++ TODO.md
@@ -8,17 +8,17 @@ Pull requests welcome
 * ~~Add specs for get_emoji~~
 * ~~Rename emoji-commit-msg class and methods~~
 * ~~Print message on successful install~~
+* ~~Ask whether they want to install~~
+* ~~Add SimpleCov~~
 * Print message on unsuccessful install/exit
 * Refactor and rename file-tasks
-* ~~Ask whether they want to install~~
 * Test asking whether they want to install
 * Test all bundle exec Thor stuff
 * Check Fileutils.cp is OK if file already exists
-* Add a rescue in to file-tasks
 * Can you do skin tone modifiers in Git emojis?
   * Add support for this if yes
 * Prompt user through install
 * Add uninstall scripts method
-* Add SimpleCov
 * Spec for file-tasks
 * Add colours to prompt messages
+* Better Readme - more emojis
--- spec/file-tasks_spec.rb
+++ spec/file-tasks_spec.rb
@@ -1,53 +1,48 @@
 require 'spec_helper'
+require 'fakefs/spec_helpers'
 
 describe EmojiCommit::Cli do
   describe '::install' do
+    xit 'is a happy path' do
+      allow(Dir).to receive(:exist?).with('.git').and_return(true)
+      allow(STDIN).to receive(:gets).and_return('y')
+      expect{EmojiCommit::Cli.new.install}.to output(/Great, installing into .git\/hooks/).to_stdout
+    end
+
     it 'will exit if there isn\'t a .git directory' do
       allow(Dir).to receive(:exist?).with('.git').and_return(false)
-      expect do
-        expect(EmojiCommit::Cli.new.install).to output('Git has not been initialised in this directory. Bye').to_stdout
-      end.to raise_error(SystemExit)
+      expect {EmojiCommit::Cli.new.install}.to output(/Git has not been initialised in this directory. Bye/).to_stdout & raise_error(SystemExit)
     end
 
     it 'will exit if user types no' do
       allow(Dir).to receive(:exist?).with('.git').and_return(true)
       allow(STDIN).to receive(:gets).and_return('n')
-      expect do
-        expect(EmojiCommit::Cli.new.install).to output('Fine whatever. Bye').to_stdout
-      end.to raise_error(SystemExit)
+      expect {EmojiCommit::Cli.new.install}.to output(/Fine whatever. Bye/).to_stdout & raise_error(SystemExit)
     end
 
     it 'will exit if user types something other than y or n' do
       allow(Dir).to receive(:exist?).with('.git').and_return(true)
       allow(STDIN).to receive(:gets).and_return('?$@*')
-      expect do
-        expect(EmojiCommit::Cli.new.install).to output('Pardon? Oh who cares. Bye').to_stdout
-      end.to raise_error(SystemExit)
+      expect{EmojiCommit::Cli.new.install}.to output(/Pardon\? Oh who cares. Bye/).to_stdout & raise_error(SystemExit)
     end
   end
 
   describe '::uninstall' do
     it 'will exit if there isn\'t a .git directory' do
       allow(Dir).to receive(:exist?).with('.git').and_return(false)
-      expect do
-        expect(EmojiCommit::Cli.new.uninstall).to output('Git has not been initialised in this directory. Bye').to_stdout
-      end.to raise_error(SystemExit)
+      expect{EmojiCommit::Cli.new.uninstall}.to output(/Git has not been initialised in this directory. Bye/).to_stdout & raise_error(SystemExit)
     end
 
     it 'will exit if user types no' do
       allow(Dir).to receive(:exist?).with('.git').and_return(true)
       allow(STDIN).to receive(:gets).and_return('n')
-      expect do
-        expect(EmojiCommit::Cli.new.uninstall).to output('Good choice. Bye').to_stdout
-      end.to raise_error(SystemExit)
+      expect{EmojiCommit::Cli.new.uninstall}.to output(/Good choice. Bye/).to_stdout & raise_error(SystemExit)
     end
 
     it 'will exit if user types something other than y or n' do
       allow(Dir).to receive(:exist?).with('.git').and_return(true)
       allow(STDIN).to receive(:gets).and_return('?$@*')
-      expect do
-        expect(EmojiCommit::Cli.new.uninstall).to output('Pardon? Oh who cares. Bye').to_stdout
-      end.to raise_error(SystemExit)
+      expect{EmojiCommit::Cli.new.uninstall}.to output(/Pardon\? Oh who cares. Bye/).to_stdout & raise_error(SystemExit)
     end
   end
 end
